5b469ec7a5925574829781a1c12a25622b25bde2
Roman Marlovits <roman.marlovits@gmail.com>
2019-11-04 07:39:57 +0100

    adjust path for script

--- scripts/import-old-data-create.sh
+++ scripts/import-old-data-create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd ../../ballkartenonline.at/docker
+cd ../../../ballkartenonline.at/docker
 docker-compose up -d ballkartenonline_db
 echo "sleep 2"
 sleep 2
-cd ../../rm-ticketing-mysql-server/_import_old_data
+cd ../../rm-ticketing/rm-ticketing-mysql-server/_import_old_data
 npm run create
 cd ../scripts
